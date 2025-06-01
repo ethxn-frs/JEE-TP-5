3dbdcb528b3ff87b06006bf90de0575a59d330e5
Ethan Francois <114412840+ethxn-frs@users.noreply.github.com>
2025-06-01 17:13:08 +0200

    Delete target directory

--- target/classes/sql/property_type.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO rent.property_type (id, designation) VALUES (0xEFBBBF31000000000000000000000000, 'Appartement');
-INSERT INTO rent.property_type (id, designation) VALUES (0xEFBBBF32000000000000000000000000, 'Maison');
